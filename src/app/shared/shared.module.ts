dcb82cb17cd243b6acb0f85a82dafda3b2756ac3
jlsn5 <jlsn5@gcloud.ua.es>
2022-04-08 11:56:06 +0200

    Continuando

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [SidebarComponent, MenuComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
