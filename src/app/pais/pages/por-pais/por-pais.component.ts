4f6bdb3047923c72a086e4222a74bcdf4d19b686
jlsn5 <jlsn5@gcloud.ua.es>
2022-04-14 15:21:24 +0200

    Siguiendo con paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,20 +12,29 @@ export class PorPaisComponent{
 
   termino: string = 'Hola Mundo';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor( private paisService: PaisService ) { }
 
 
-  buscar(){
+  buscar( termino: string ){
     this.hayError = false;
+    this.termino = termino;
 
     this.paisService.buscarPais( this.termino )
       .subscribe( (paises) => {
         console.log(paises);
 
+        this.paises = paises;
+
       }, (err) => {
         this.hayError = true;
+        this.paises = [];
       });
+  }
+
+  sugerencias( termino: string){
+    console.log(termino)
   }
 
 }
