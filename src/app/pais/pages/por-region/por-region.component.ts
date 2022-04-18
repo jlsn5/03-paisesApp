66e955244b1637777402054cd6fd185bc4ba09d8
jlsn5 <jlsn5@gcloud.ua.es>
2022-04-18 22:21:49 +0200

    siguiendo

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [ `
+    button {
+      margin-right: 5px;
+    }
+
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['Europe', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC',];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
